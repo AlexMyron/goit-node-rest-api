f2e10320fe3d18c0e92af1c2631080c89242a1bf
Olek Myron <myrondell@gmail.com>
2025-03-04 21:57:07 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,18 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().trim().min(2).max(50).required(),
+  email: Joi.string().trim().email().required(),
+  phone: Joi.string()
+    .trim()
+    .pattern(/^\+?[0-9\s\-()]{7,20}$/)
+    .required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().trim().min(2).max(50),
+  email: Joi.string().trim().email(),
+  phone: Joi.string()
+    .trim()
+    .pattern(/^\+?[0-9\s\-()]{7,20}$/),
+}).min(1);
